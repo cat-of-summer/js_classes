cbca0d76d5e019f77717bbb89b3aad02558598d0
cat-of-summer <xramic39@mail.ru>
2024-10-21 23:41:13 +0200

    n

---
+++ st_system/st_system.js
@@ -0,0 +1,22 @@
+class st_system {
+    
+    static object_merge(...OBJECTS) {
+        let new_object = {};
+        OBJECTS.forEach(old_object => {
+            let recursive = (field, old_object, new_object) => {
+                if (typeof old_object[field] == "object")
+                    for (let content in old_object[field]) {
+                        console.log(content);
+                        if (!new_object[field]) new_object[field] = {};
+                        recursive(content, old_object[field], new_object[field]);
+                    }
+                else
+                    new_object[field] = old_object[field];
+            }
+            for (let field in old_object)
+                recursive(field, old_object, new_object)
+        });
+        return new_object
+    }
+
+}
