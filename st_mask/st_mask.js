7b6c1f7dc162cf2f5170ac6c26bacb59318f490f
cat-of-summer <xramic39@mail.ru>
2025-03-07 23:26:55 +0200

    .

--- st_mask/st_mask.js
+++ st_mask/st_mask.js
@@ -10,35 +10,37 @@ class st_mask {
         let static_mask_index = 0;
         let dynamic_mask_index = 0;
     
-        for (let char of input_string)
-            while (dynamic_mask_index + static_mask_index < mask.length)
-                if (!mask[dynamic_mask_index + static_mask_index][1])
-                    result += mask[dynamic_mask_index + static_mask_index++][0];
+        for (let char of input_string) {
+            while (dynamic_mask_index + static_mask_index < mask.length) {
+                let [value, is_regexp, filler] = mask[dynamic_mask_index + static_mask_index];
+        
+                if (!is_regexp) result += value, static_mask_index++;
                 else {
-                    if (new RegExp(`^${mask[dynamic_mask_index + static_mask_index][0]}$`).test(char)) {
-                        result += char;
-                        dynamic_mask_index++;
-                    }
+                    if (new RegExp(`^${value}$`).test(char)) result += char, dynamic_mask_index++;
                     break;
                 }
+            }
+        }
         
         if (this.#default_filler && this.#placeholder)
-            while (dynamic_mask_index + static_mask_index < mask.length) {
-                let [ value, is_regexp, default_filler] = mask[dynamic_mask_index + static_mask_index++];
-                result += is_regexp ? default_filler : value;
-            }
-            
+            while (dynamic_mask_index + static_mask_index < mask.length)
+                result += mask[dynamic_mask_index + static_mask_index++][1] 
+                    ? mask[dynamic_mask_index + static_mask_index - 1][2] 
+                    : mask[dynamic_mask_index + static_mask_index - 1][0];
+        else if (!dynamic_mask_index) result = '';
+        else if (dynamic_mask_index + static_mask_index < mask.length && !mask[dynamic_mask_index + static_mask_index][1]) 
+            result += mask[dynamic_mask_index + static_mask_index++][0];
+        
         return [result, dynamic_mask_index, mask.filter(mask_char => mask_char[1]).length == dynamic_mask_index];
     }
 
     #return_best_mask(raw_value) {
-        let best_mask = null;
-
-        this.#mask_array.map((mask) => this.#apply_mask(mask, raw_value)).forEach((value, index) => {
-            if (!best_mask || value[1] > best_mask[0][1]) best_mask = [value, index];
-        });
-
-        return best_mask;
+        return this.#mask_array
+            .map(mask => this.#apply_mask(mask, raw_value))
+            .reduce((best, value, index) => (!best || value[1] > best[0][1] 
+                ? [value, index] 
+                : best)
+            , null);
     }
 
     #prepare_mask(mask) {
@@ -46,31 +48,37 @@ class st_mask {
             .split(/({{.*?}.*?})/)
             .filter(Boolean)
             .map(value => {
-                let inner_value = value.match(/^{{(.*?)}.*?}$/);
-                let filler = value.match(/^{{.*?}=(.*?)}$/);
-                return [
-                    inner_value
-                        ? inner_value[1] 
-                        : value,
-                    !!inner_value,
-                    filler
-                        ? filler[1]
-                        : this.#default_filler
-                ];
+                let [, inner_value] = value.match(/^{{(.*?)}.*?}$/) || [];
+                let [, filler] = value.match(/^{{.*?}=(.*?)}$/) || [];
+                return [inner_value || value, !!inner_value, filler || this.#default_filler];
             })
     }
 
-    #refresh_masks() {
-        this.#inputs.forEach(input_object => {
-            input_object.best_mask = this.#return_best_mask(input_object.value);
-            input_object.input.value = input_object.best_mask[0][0];
+    #refresh_mask(input_object = null) {
+        if (this.#is_mask_array_empty()) {
+            console.warn('Попытка обновить маску после удаления правил', this);
+            return;
+        }
+
+        (input_object ? [input_object] : this.#inputs).forEach(obj => {
+            obj.best_mask = this.#return_best_mask(obj.value);
+            obj.input.value = obj.best_mask[0][0];
+            Object.assign(obj.input.dataset, {
+                mask_id: obj.best_mask[1], 
+                progress: obj.best_mask[0][1], 
+                is_full: obj.best_mask[0][2]
+            });
         });
+    }
+
+    #is_mask_array_empty() {
+        return !this.#mask_array.some(item => !item != null);
     }
 
     add_mask(mask) {
         this.#mask_array.push(this.#prepare_mask(mask));
 
-        this.#refresh_masks();
+        this.#refresh_mask();
 
         return this.#mask_array.length - 1;
     }
@@ -78,7 +86,7 @@ class st_mask {
     remove_mask(index) {
         delete this.#mask_array[index];
 
-        this.#refresh_masks();
+        this.#refresh_mask();
     }
 
     set_masks(masks) {
@@ -86,19 +94,14 @@ class st_mask {
             return this.#prepare_mask(mask);
         });
 
-        this.#refresh_masks();
+        this.#refresh_mask();
     }
 
     constructor(param) {
         let $this = this;
         
-        $this.#default_filler ??= param.default_filler;
-        $this.#placeholder ??= param.placeholder;
-
-        if (param.masks) {
-            $this.set_masks(param.masks);
-            $this.#refresh_masks();
-        }
+        $this.#default_filler = param.default_filler ?? $this.#default_filler;
+        $this.#placeholder = param.placeholder ?? $this.#placeholder;
 
         document.querySelectorAll(param.selector).forEach(input => {
 
@@ -106,42 +109,49 @@ class st_mask {
                 input: input,
                 value: input.value.split().filter(Boolean)
             };
-                        
+            
             input.addEventListener("input", (event) => {
                 event.preventDefault();
                 
-                if ($this.#mask_array.length) {
-                    if (event.data === null) {
-                        let new_raw_value = [];
+                if (this.#is_mask_array_empty()) {
+                    console.error('Нет масок для этого поля.', this);
+                    return;
+                }
 
-                        for (let char of input_object.value) {
-                            new_raw_value.push(char);
-                        
-                            if (this.#apply_mask(this.#mask_array[input_object.best_mask[1]], new_raw_value)[1] == input_object.best_mask[0][1]) {
-                                new_raw_value.pop();
-                                break;
-                            }
-                        }
-                        
-                        input_object.value = new_raw_value;
-                    } else 
-                        input_object.value.push(...event.data);      
+                if (event.data === null) {
+                    let new_raw_value = [];
 
-                    input_object.best_mask = $this.#return_best_mask(input_object.value);
-                    input.value = input_object.best_mask[0][0];
+                    for (let char of input_object.value) {
+                        new_raw_value.push(char);
                     
-                    if (typeof param.callback === 'function')
-                        param.callback(input, {
-                            mask_id: input_object.best_mask[1], 
-                            progress: input_object.best_mask[0][1], 
-                            is_full: input_object.best_mask[0][2]
-                        });
+                        if (this.#apply_mask(this.#mask_array[input_object.best_mask[1]], new_raw_value)[1] == input_object.best_mask[0][1]) {
+                            new_raw_value.pop();
+                            break;
+                        }
+                    }
+                    
+                    input_object.value = new_raw_value;
                 } else
-                    console.error('Нет масок для этого input-поля.');
+                    input_object.value.push(...event.data);      
+
+                $this.#refresh_mask(input_object);
+                
+                if (typeof param.on_input === 'function')
+                    param.on_input(input, {
+                        mask_id: input_object.best_mask[1], 
+                        progress: input_object.best_mask[0][1], 
+                        is_full: input_object.best_mask[0][2]
+                    });
             });
+
+            if (typeof param.on_init === 'function')
+                param.on_init(input);
 
             $this.#inputs.push(input_object);
         });
+
+        if (param.masks)
+            $this.set_masks(param.masks);
     }
 
 }
