23ae4ec5627535fb94e0e64b73db84eb2fcd3fce
cat-of-summer <163355316+cat-of-summer@users.noreply.github.com>
2025-04-25 16:47:04 +0200

    test

--- st_validator/st_validator.js
+++ st_validator/st_validator.js
@@ -1,8 +1,11 @@
 class st_validator {
 
+	#validator = null;
+    #targets = null;
+
     constructor(param) {
 
-        let validator_f = function (input) {
+        this.#validator = function (input) {
             if ((param.validator ? param.validator(input) : true) &&
                 ((() => {
                 if (param.regexp) {
@@ -19,23 +22,31 @@ class st_validator {
                     return param.attribute ? input.hasAttribute(param.attribute) : true;
                 }
             })()))
-                param.on_valid(input)
+                param.on_valid(input);
             else
-                param.on_invalid(input)
+                param.on_invalid(input);
+
+            if (param.after_check) param.after_check(input);
         };
 
-        document.querySelectorAll(param.selector).forEach(input => {
-            if (param.onload === true) validator_f(input);
+        this.#targets = document.querySelectorAll(param.selector);
 
+        if (param.onload === true) this.apply;
+
+        this.#targets.forEach(input => {
             (param.events ?? [
                 'input',
                 'blur'
             ]).forEach(event => {
                 input.addEventListener(event, () => {
-                    validator_f(input)
+                    this.#validator(input)
                 });
             });
         });
-  
+    }
+
+	apply(selector = null) {
+        (selector ? document.querySelectorAll(selector) : this.#targets)
+            .forEach(this.#validator, this);
     }
 }
